02916e81ad4861cb903196fd9336a7ffe5fe50d4
frodo <eugene@moscow.ctm.ru>
2016-01-22 16:18:06 +0300

    add some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Candidate.create(name: 'Петров', fee: 100000)
+Candidate.create(name: 'Иванов', fee: 10000)
 Skill.create(name: 'C++')
+Skill.create(name: 'Java')
+Skill.create(name: 'Scala')
+Skill.create(name: 'Python')
+
 Vacancy.create(name: 'Разработчик C++', fee: 100000.0, expire_at: Time.now + 100.days, added_at: Time.now, contact_info: 'me@hr.example.com')
+Vacancy.create(name: 'Разработчик Ruby', fee: 20000.0, expire_at: Time.now + 100.days, added_at: Time.now, contact_info: 'me@hr.example.ru')
